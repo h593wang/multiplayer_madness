d988719512861f1fff551e11d8b21378ad4e4544
Kevin Trieu <kevin@enzuzo.com>
2023-08-13 00:54:19 -0400

    Another CI fix

--- hathora-deploy.sh
+++ hathora-deploy.sh
@@ -1,10 +1,10 @@
 #! /bin/bash
 
-tar -czf server.tar.gz server.pck Dockerfile
+tar -czf server.tgz server.pck Dockerfile
 
 hathora-cloud deploy \
     --appId app-2fbbeb0b-0e85-4ee3-ad3e-9320f0980d1b \
-    --file ./server.tar.gz \
+    --file ./server.tgz \
     --roomsPerProcess 1 \
     --planName tiny \
     --transportType udp \
