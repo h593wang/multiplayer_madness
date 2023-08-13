d2adc506234c4d760f5a8c16e00456d8f3e32f19
Bobby Wang <bobby.wang25@gmail.com>
2023-08-13 11:57:46 -0700

    change server instantiation structure

--- client/startup.gd
+++ client/startup.gd
@@ -1,6 +1,7 @@
 extends Node2D
 
 var client_peer: ENetMultiplayerPeer
+const server = preload('res://server/server.tscn')
 
 func was_started_with_server():
 	return '--server' in OS.get_cmdline_user_args()
@@ -8,8 +9,8 @@ func was_started_with_server():
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	if was_started_with_server():
-		get_tree().change_scene_to_file('res://server/server.tscn')
+		get_tree().root.add_child(server.instantiate())
 	else:
 		client_peer = ENetMultiplayerPeer.new()
-		client_peer.create_client("1.proxy.hathora.dev", 44765)
+		client_peer.create_client("1.proxy.hathora.dev", 32383)
 		multiplayer.multiplayer_peer = client_peer
