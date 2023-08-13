fde6af0cbe2ff9d02e3dee02f52b5f56cc0ca1c7
Kevin Trieu <kevin@enzuzo.com>
2023-08-13 01:20:46 -0400

    Fix server name

--- client/startup.gd
+++ client/startup.gd
@@ -6,11 +6,12 @@ func was_started_with_server():
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	if was_started_with_server():
-		get_tree().change_scene_to_file('res://server/test_server.tscn')
+		get_tree().change_scene_to_file('res://server/server.tscn')
 	
 	var client_peer = ENetMultiplayerPeer.new()
 	client_peer.create_client("1.proxy.hathora.dev", 41487)
 	multiplayer.multiplayer_peer = client_peer
+	
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
