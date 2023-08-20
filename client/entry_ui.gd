913a2587075bded1e09414c265de117705656999
Kevin Trieu <kevin.trieu5813@gmail.com>
2023-08-19 20:36:33 -0400

    Fix warnings and refactor cutscene code

--- client/entry_ui.gd
+++ client/entry_ui.gd
@@ -1,69 +1,80 @@
 extends Control
 
-var intro_stage = 0
-var end_stage = -1
+var stage = 0
+
+enum CutsceneState {
+	INTRO,
+	ENDING,
+	DONE,
+}
+
+var state = CutsceneState.INTRO
+
+var intro_images = [
+	preload('res://common/frame1-1.png'),
+	preload('res://common/frame1-2.png'),
+	preload('res://common/frame2-1.png'),
+	preload('res://common/frame2-2.png'),
+	preload('res://common/frame3-1.png'),
+	preload('res://common/frame3-1.png'),
+]
+
+var intro_texts = [
+	"*Flick*",
+	"Woah, gramps has so much cool stuff",
+	"Guys look! This computer still turns on.",
+	"WHAT'S HAPPENING!?",
+	"Is everyone okay?\nIt looks like we're in the internet and we need to...",
+	"BREAK FREE!",
+]
+
+var ending_images = [
+	load("res://common/frame4-1.png"),
+	load("res://common/frame4-2.png"),
+]
+
+var ending_texts = [
+	"We made it out!",
+	"I'm never using a computer ever again."
+]
+
+var NUM_INTRO_STAGES = 6
+var NUM_ENDING_STAGES = 2
+
+func advance():
+	if state == CutsceneState.DONE:
+		return
+	
+	stage += 1
+	if state == CutsceneState.INTRO and stage >= NUM_INTRO_STAGES:
+		state = CutsceneState.DONE
+	elif state == CutsceneState.ENDING and stage >= NUM_ENDING_STAGES:
+		state = CutsceneState.DONE
+		
+	render()
 
 func _input(event):
 	if event is InputEventMouseButton:
 		if event.button_index == MOUSE_BUTTON_LEFT and event.is_pressed():
-			if end_stage != -1:
-				advance_ending()
-			else:
-				advance_intro()
+			advance()
+			
+func render():
+	if state == CutsceneState.INTRO:
+		visible = true
+		$cutscene_image.texture = intro_images[stage]
+		$HBoxContainer/Label.text = intro_texts[stage]
+	elif state == CutsceneState.ENDING:
+		visible = true
+		$cutscene_image.texture = ending_images[stage]
+		$HBoxContainer/Label.text = ending_texts[stage]
+	elif state == CutsceneState.DONE:
+		visible = false
+			
+func show_ending():
+	state = CutsceneState.ENDING
+	stage = 0
+	render()
 
 func _ready():
-	Globals.game_win.connect(advance_ending)
+	Globals.game_win.connect(show_ending)
 
-func show_ending():
-	end_stage = 0
-	advance_ending()
-
-func advance_intro():
-	intro_stage += 1
-	if intro_stage > 5:
-		visible = false
-		
-	var image = Image.load_from_file("res://common/frame1-1.png")
-	match (intro_stage):
-		1: 
-			image = Image.load_from_file("res://common/frame1-2.png")
-		2: 
-			image = Image.load_from_file("res://common/frame2-1.png")
-		3: 
-			image = Image.load_from_file("res://common/frame2-2.png")
-		_: 
-			image = Image.load_from_file("res://common/frame3-1.png")
-	$cutscene_image.texture = ImageTexture.create_from_image(image)
-	
-	var text = "*Flick*"
-	match (intro_stage):
-		1: 
-			text = "Woah, gramps has so much cool stuff"
-		2: 
-			text = "Guys look! This computer still turns on."
-		3: 
-			text = "WHAT'S HAPPENING!?"
-		4: 
-			text = "Is everyone okay?\nIt looks like we're in the internet and we need to..."
-		5:
-			text = "BREAK FREE!"
-	$HBoxContainer/Label.text = text
-
-func advance_ending():
-	visible = true
-	if end_stage > 1:
-		visible = false
-	
-	
-	var image = Image.load_from_file("res://common/frame4-1.png")
-	match (end_stage):
-		1: 
-			image = Image.load_from_file("res://common/frame4-2.png")
-	$cutscene_image.texture = ImageTexture.create_from_image(image)
-	
-	var text = "We made it out!"
-	match (end_stage):
-		1: 
-			text = "I'm never using a computer ever again."
-	$HBoxContainer/Label.text = text
-	end_stage += 1
